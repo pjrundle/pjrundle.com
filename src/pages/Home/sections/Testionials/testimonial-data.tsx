c32a4eab07b309f57923782ae36432a2c07f47ee
pjrundle <petejrundle@gmail.com>
2026-02-24 20:56:07 +0000

    style: testimonial trim

--- src/pages/Home/sections/Testionials/testimonial-data.tsx
+++ src/pages/Home/sections/Testionials/testimonial-data.tsx
@@ -1,22 +1,5 @@
 export const testimonialData = [
   {
-    name: "Will McInnes",
-    meta: "CMO | SER Group (formerly Brandwatch)",
-    profileImgSrc: "/profile-images/will-mcinnes.jpg",
-    highlight: "I’d work with Pete again in a heartbeat.",
-    text: (
-      <>
-        <p>
-          Pete is a rock solid one-man-army that led multiple major website
-          overhauls and evolutions for us at Brandwatch. A strong collaborator,
-          with a fine balance of design and tech, Pete’s work ethic was epic and
-          his commitment to quality was honestly inspiring. I’d work with Pete
-          again in a heartbeat.
-        </p>
-      </>
-    ),
-  },
-  {
     name: "Andy Hook",
     meta: "Software Engineer | Radix/Work OS",
     profileImgSrc: "/profile-images/andy-hook.jpg",
@@ -48,18 +31,15 @@ export const testimonialData = [
     text: (
       <>
         <p>
-          Pete was initially hired in as someone to take the lead in the
-          business on UX. The first task was joining the product design team to
-          help craft our next generation set of tools. It’s fair to say Pete was
-          an instant star in the business and really wowed everyone with his
-          combination of speed and quality. Not only that Pete turned out to be
-          the equal of our best developers from the start, and played an active
-          role in not only designing the UX but delivering really great code to
-          implement it. During the whole course of Pete’s time with us, his
-          involvement in the business became stronger. Without a doubt with his
-          eye for quality design, passion for great architecture and pure coding
-          ability, one of the most valuable team members you could hope to work
-          with.
+          It’s fair to say Pete was an instant star in the business and really
+          wowed everyone with his combination of speed and quality. Not only
+          that Pete turned out to be the equal of our best developers from the
+          start, and played an active role in not only designing the UX but
+          delivering really great code to implement it. During the whole course
+          of Pete’s time with us, his involvement in the business became
+          stronger. Without a doubt with his eye for quality design, passion for
+          great architecture and pure coding ability, one of the most valuable
+          team members you could hope to work with.
         </p>
       </>
     ),
@@ -72,15 +52,12 @@ export const testimonialData = [
     text: (
       <>
         <p>
-          Pete worked for me specialising in design and front end technology.
-          Redesigning a desktop app and designing a mobile app for a client
-          showed that Pete has the rare qualities of design and engineering in
-          one person. Pete is very quick to pick up and run with technical
-          concepts and is super organised. His designs always please and hit the
-          spot and show a thorough appreciation of user centred design. Also he
-          is highly personable, polite and dedicated to his roles, we have
-          worked and continue to work together on many projects since Pete went
-          on to greater things.
+          Pete has the rare qualities of design and engineering in one person –
+          very quick to pick up and run with technical concepts and is super
+          organised. His designs always please and hit the spot and show a
+          thorough appreciation of user centred design. He is highly personable,
+          polite and dedicated to his roles, we have worked and continue to work
+          together on many projects.
         </p>
       </>
     ),
@@ -102,6 +79,23 @@ export const testimonialData = [
           as you know what you deliver will always be a level above where you
           even expected it to be in the first place. A true pleasure to work
           with. No doubt our paths will cross in the future.
+        </p>
+      </>
+    ),
+  },
+  {
+    name: "Will McInnes",
+    meta: "CMO | SER Group (formerly Brandwatch)",
+    profileImgSrc: "/profile-images/will-mcinnes.jpg",
+    highlight: "I’d work with Pete again in a heartbeat.",
+    text: (
+      <>
+        <p>
+          Pete is a rock solid one-man-army that led multiple major website
+          overhauls and evolutions for us at Brandwatch. A strong collaborator,
+          with a fine balance of design and tech, Pete’s work ethic was epic and
+          his commitment to quality was honestly inspiring. I’d work with Pete
+          again in a heartbeat.
         </p>
       </>
     ),
