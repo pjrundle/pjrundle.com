f475cca675205c46056c7a89951e4fef273e2939
pjrundle <petejrundle@gmail.com>
2026-04-04 17:04:06 +0100

    initial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import { Icon } from "@theme-os/react";
+import { Button, Icon } from "@theme-os/react";
 import { cn } from "@theme-os/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { HiMenu } from "react-icons/hi";
 import { IoLogoGithub } from "react-icons/io";
 import { RiLinkedinFill } from "react-icons/ri";
 import { Link } from "wouter";
@@ -8,6 +11,31 @@ import { PJRGlyph } from "../assets/PJRGlyph.tsx";
 
 const LOGO_WIDTH_MOBILE = 60;
 const LOGO_WIDTH_LG = 74;
+
+const MenuIcon = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button
+      aria-label="Menu"
+      className={cn(
+        `w:${LOGO_WIDTH_MOBILE}px w:${LOGO_WIDTH_LG - 10}px@lg`,
+        "inline-block rel",
+        "bg:color-gray-50 color:color-gray-900",
+        "bg:color-gray-100:hover",
+        "bl:border-b bt:border-b",
+        "cursor:pointer",
+      )}
+      onClick={onClick}
+    >
+      <span className="aspect:1/1 block" />
+
+      <div className="abs-center w:32.5% flex flex-col gap-y:5px">
+        <span className="block w:100% h:1px bg:color-gray-700" />
+        <span className="block w:100% h:1px bg:color-gray-700" />
+        <span className="block w:100% h:1px bg:color-gray-700" />
+      </div>
+    </button>
+  );
+};
 
 const Logo = () => {
   return (
@@ -51,7 +79,7 @@ const SocialLink = ({
         target="_blank"
         aria-label={label}
         className={cn(
-          "w:50% p:2x p:2.5x@md",
+          "w:50% p:2x p:3x@md",
           "z:1:focus-visible",
           "{bg:color-gray-100;color:color-gray-700}:hover",
           "flex align-items:center justify-content:center",
@@ -65,37 +93,162 @@ const SocialLink = ({
   );
 };
 
-export const Header = () => {
+const NavSection = ({
+  label,
+  links,
+}: {
+  label: string;
+  links: { href: string; label: string }[];
+}) => {
   return (
-    <header className="sticky top:0 z:100">
-      <div className="page-gutter">
-        <div className="page-container-lg bg:color-gray-0 z:100 bb:border-b bx:border-b flex justify-content:space-between">
-          <Logo />
+    <div className="w:260px">
+      <div className="typestyle-meta f:10 mb:5x">{label}</div>
+      <div className="flex flex-direction:column f:12">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="typestyle-copy pb:1x"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-          <div className="flex gap:6x">
-            <nav className="color:color-gray-500 f:17 f:20@lg flex">
-              <ul
-                className={cn(
-                  "flex bl:border-b bt:border-b bg:color-gray-0",
-                  `w:${LOGO_WIDTH_MOBILE + 10}px w:${LOGO_WIDTH_LG + 10}px@lg`,
-                )}
-              >
-                <SocialLink
-                  icon={RiLinkedinFill}
-                  href="https://www.linkedin.com/in/pete-rundle/"
-                  label="View Linkedin profile"
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <header className="sticky top:0 z:100">
+        <div className="page-gutter rel z:1">
+          <div className="page-container-lg bg:color-gray-0 z:100 bb:border-b bx:border-b flex justify-content:space-between">
+            <Logo />
+
+            <div className="flex gap-x:6x">
+              <div className="flex f:12 align-items:center gap-x:3x">
+                {/* <div className="flex r:20px b:border-b overflow:hidden">
+                <Link
+                  href="/approach"
+                  className="typestyle-meta flex align-items:center px:4x py:2x bxborder-b"
+                >
+                  <span>/ Approach</span>
+                </Link>
+                <Link
+                  href="/approach"
+                  className="typestyle-meta flex align-items:center px:4x py:2x"
+                >
+                  <span>/ Process</span>
+                </Link>
+              </div> */}
+
+                {/* <Badge>Approach</Badge>
+              <Badge>Process</Badge> */}
+
+                <Button
+                  label="Menu"
+                  icon={HiMenu}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
                 />
-                <SocialLink
-                  icon={IoLogoGithub}
-                  className="bl:border-b"
-                  href="https://github.com/pjrundle"
-                  label="View GitHub profile"
-                />
-              </ul>
-            </nav>
+              </div>
+
+              <nav className="color:color-gray-500 f:17 f:19@lg flex">
+                <ul
+                  className={cn(
+                    "hidden!",
+                    "flex blborder-b btborder-b bg:color-gray-0 mr:1.5x",
+                    `w:${LOGO_WIDTH_MOBILE + 10}px w:${LOGO_WIDTH_LG + 10}px@lg`,
+                  )}
+                >
+                  <SocialLink
+                    icon={RiLinkedinFill}
+                    href="https://www.linkedin.com/in/pete-rundle/"
+                    label="View Linkedin profile"
+                  />
+                  <SocialLink
+                    icon={IoLogoGithub}
+                    className="blborder-b pr:6x!"
+                    href="https://github.com/pjrundle"
+                    label="View GitHub profile"
+                  />
+                </ul>
+
+                {/* <MenuIcon onClick={() => setIsMenuOpen(!isMenuOpen)} /> */}
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              key="menu-backdrop"
+              initial={{ opacity: 0, y: "-100%" }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: "-100%" }}
+              transition={{ duration: 0.2 }}
+              className={cn(
+                "abs bottom:-270px left:0 right:0 height:270px bd:blur(30px) bb:border-b shadowshadow-xl",
+              )}
+            >
+              <div className="abs-fill bg:color-gray-0 opacity:0.75" />
+
+              <div className="rel mt:13x">
+                <div className="page-gutter">
+                  <div className="page-container-lg">
+                    <div className="flex gap-x:16x">
+                      <NavSection
+                        label="Products / Systems"
+                        links={[
+                          { href: "/projects/theme-os", label: "ThemeOS" },
+                          { href: "/projects/wombat", label: "Wombat" },
+                        ]}
+                      />
+                      <NavSection
+                        label="Case Studies"
+                        links={[
+                          { href: "/projects/cloud-iq", label: "Cloud.IQ" },
+                          { href: "/projects/brandwatch", label: "Brandwatch" },
+                        ]}
+                      />
+                      <NavSection
+                        label="Writing"
+                        links={[
+                          {
+                            href: "/labs/theme-os",
+                            label: "Figma: the right place for design systems?",
+                          },
+                          {
+                            href: "/labs/wombat",
+                            label: "Designing in code without friction",
+                          },
+                          {
+                            href: "/labs/brandwatch",
+                            label: "No-code - easier than learning tailwind?",
+                          },
+                          {
+                            href: "/labs/brandwatch",
+                            label: "What is a design engineer?",
+                          },
+                        ]}
+                      />
+                      <div className="">
+                        <div className="typestyle-meta f:10 mb:5x">Contact</div>
+                        <div className="typestyle-copy f:12">
+                          hello@pjrundle.com
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </header>
+    </>
   );
 };
